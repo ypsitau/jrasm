5fa858d2c5026dbe42a96407e3e3294e83c41ea5
ypsitau <ypsitau@nifty.com>
2018-09-13 13:03:13 +0900

    writing instruction table

--- src/jrasm.cpp
+++ src/jrasm.cpp
@@ -19,18 +19,20 @@ public:
 	static InstInfo *Syntax_INH(const String &symbol, UInt8 codeINH);
 	static InstInfo *Syntax_ACC_ACC(const String &symbol, UInt8 codeACC_A, UInt8 codeACC_B);
 	static InstInfo *Syntax_IDX_EXT(const String &symbol, UInt8 codeIDX, UInt8 codeEXT);
-	static InstInfo *Syntax_ACC_IMM_DIR_IDX_EXT(
-		const String &symbol,
-		UInt8 codeIMM_A, UInt8 codeDIR_A, UInt8 codeIDX_A, UInt8 codeEXT_A,
-		UInt8 codeIMM_B, UInt8 codeDIR_B, UInt8 codeIDX_B, UInt8 codeEXT_B);
-	static InstInfo *Syntax_ACC_DIR_IDX_EXT	(
-		const String &symbol,
-		UInt8 codeDIR_A, UInt8 codeIDX_A, UInt8 codeEXT_A,
-		UInt8 codeDIR_B, UInt8 codeIDX_B, UInt8 codeEXT_B);
+	static InstInfo *Syntax_DIR_IDX_EXT(
+		const String &symbol, UInt8 codeDIR, UInt8 codeIDX, UInt8 codeEXT);
 	static InstInfo *Syntax_DIR_IDX_IMM_EXT(
 		const String &symbol, UInt8 codeDIR, UInt8 codeIDX, UInt8 codeIMM, UInt8 codeEXT);
 	static InstInfo *Syntax_ACC_ACC_IDX_EXT(
 		const String &symbol, UInt8 codeACC_A, UInt8 codeACC_B, UInt8 codeIDX, UInt8 codeEXT);
+	static InstInfo *Syntax_AxB_DIR_IDX_EXT	(
+		const String &symbol,
+		UInt8 codeDIR_A, UInt8 codeIDX_A, UInt8 codeEXT_A,
+		UInt8 codeDIR_B, UInt8 codeIDX_B, UInt8 codeEXT_B);
+	static InstInfo *Syntax_AxB_IMM_DIR_IDX_EXT(
+		const String &symbol,
+		UInt8 codeIMM_A, UInt8 codeDIR_A, UInt8 codeIDX_A, UInt8 codeEXT_A,
+		UInt8 codeIMM_B, UInt8 codeDIR_B, UInt8 codeIDX_B, UInt8 codeEXT_B);
 };
 
 //-----------------------------------------------------------------------------
@@ -59,9 +61,9 @@ InstInfoMap::~InstInfoMap()
 void InstInfoMap::Initialize()
 {
 	Add(InstInfo::Syntax_ACC				("aba", 0x1b));
-	Add(InstInfo::Syntax_ACC_IMM_DIR_IDX_EXT("adc", 0x89, 0x99, 0xa9, 0xb9, 0xc9, 0xd9, 0xe9, 0xf9));
-	Add(InstInfo::Syntax_ACC_IMM_DIR_IDX_EXT("add", 0x8b, 0x9b, 0xab, 0xbb, 0xcb, 0xdb, 0xeb, 0xfb));
-	Add(InstInfo::Syntax_ACC_IMM_DIR_IDX_EXT("and", 0x84, 0x94, 0xa4, 0xb4, 0xc4, 0xd4, 0xe4, 0xf4));
+	Add(InstInfo::Syntax_AxB_IMM_DIR_IDX_EXT("adc", 0x89, 0x99, 0xa9, 0xb9, 0xc9, 0xd9, 0xe9, 0xf9));
+	Add(InstInfo::Syntax_AxB_IMM_DIR_IDX_EXT("add", 0x8b, 0x9b, 0xab, 0xbb, 0xcb, 0xdb, 0xeb, 0xfb));
+	Add(InstInfo::Syntax_AxB_IMM_DIR_IDX_EXT("and", 0x84, 0x94, 0xa4, 0xb4, 0xc4, 0xd4, 0xe4, 0xf4));
 	Add(InstInfo::Syntax_ACC_ACC_IDX_EXT	("asl", 0x48, 0x58, 0x68, 0x78));
 	Add(InstInfo::Syntax_ACC_ACC_IDX_EXT	("asr", 0x47, 0x57, 0x67, 0x77));
 	Add(InstInfo::Syntax_REL				("bcc", 0x24));
@@ -85,26 +87,26 @@ void InstInfoMap::Initialize()
 	Add(InstInfo::Syntax_INH				("cli", 0x0e));
 	Add(InstInfo::Syntax_ACC_ACC_IDX_EXT	("clr", 0x4f, 0x5f, 0x6f, 0x7f));
 	Add(InstInfo::Syntax_INH				("clv", 0x0a));
-	Add(InstInfo::Syntax_ACC_IMM_DIR_IDX_EXT("cmp", 0x81, 0x91, 0xa1, 0xb1, 0xc1, 0xd1, 0xe1, 0xf1));
+	Add(InstInfo::Syntax_AxB_IMM_DIR_IDX_EXT("cmp", 0x81, 0x91, 0xa1, 0xb1, 0xc1, 0xd1, 0xe1, 0xf1));
 	Add(InstInfo::Syntax_ACC_ACC_IDX_EXT	("com", 0x43, 0x53, 0x63, 0x73));
 	Add(InstInfo::Syntax_DIR_IDX_IMM_EXT	("cpx", 0x9c, 0xac, 0x8c, 0xbc));
 	Add(InstInfo::Syntax_INH				("daa", 0x19));
 	Add(InstInfo::Syntax_ACC_ACC_IDX_EXT	("dec", 0x4a, 0x5a, 0x6a, 0x7a));
 	Add(InstInfo::Syntax_INH				("des", 0x34));
 	Add(InstInfo::Syntax_INH				("dex", 0x09));
-	Add(InstInfo::Syntax_ACC_IMM_DIR_IDX_EXT("eor", 0x88, 0x98, 0xa8, 0xb8, 0xc8, 0xd8, 0xe8, 0xf8));
+	Add(InstInfo::Syntax_AxB_IMM_DIR_IDX_EXT("eor", 0x88, 0x98, 0xa8, 0xb8, 0xc8, 0xd8, 0xe8, 0xf8));
 	Add(InstInfo::Syntax_ACC_ACC_IDX_EXT	("inc", 0x4c, 0x5c, 0x6c, 0x7c));
 	Add(InstInfo::Syntax_INH				("ins", 0x31));
 	Add(InstInfo::Syntax_INH				("inx", 0x08));
 	Add(InstInfo::Syntax_IDX_EXT			("jmp", 0x6e, 0x7e));
 	Add(InstInfo::Syntax_IDX_EXT			("jsr", 0xad, 0xbd));
-	Add(InstInfo::Syntax_ACC_IMM_DIR_IDX_EXT("lda", 0x86, 0x96, 0xa6, 0xb6, 0xc6, 0xd6, 0xe6, 0xf6));
+	Add(InstInfo::Syntax_AxB_IMM_DIR_IDX_EXT("lda", 0x86, 0x96, 0xa6, 0xb6, 0xc6, 0xd6, 0xe6, 0xf6));
 	Add(InstInfo::Syntax_DIR_IDX_IMM_EXT	("lds", 0x9e, 0xae, 0x8e, 0xbe));
 	Add(InstInfo::Syntax_DIR_IDX_IMM_EXT	("ldx", 0xde, 0xee, 0xce, 0xfe));
 	Add(InstInfo::Syntax_ACC_ACC_IDX_EXT	("lsr", 0x44, 0x54, 0x64, 0x74));
 	Add(InstInfo::Syntax_ACC_ACC_IDX_EXT	("neg", 0x40, 0x50, 0x60, 0x70));
 	Add(InstInfo::Syntax_INH				("nop", 0x01));
-	Add(InstInfo::Syntax_ACC_IMM_DIR_IDX_EXT("ora", 0x8a, 0x9a, 0xaa, 0xba, 0xca, 0xda, 0xea, 0xfa));
+	Add(InstInfo::Syntax_AxB_IMM_DIR_IDX_EXT("ora", 0x8a, 0x9a, 0xaa, 0xba, 0xca, 0xda, 0xea, 0xfa));
 	Add(InstInfo::Syntax_ACC_ACC			("psh", 0x36, 0x37));
 	Add(InstInfo::Syntax_ACC_ACC			("pul", 0x32, 0x32));
 	Add(InstInfo::Syntax_ACC_ACC_IDX_EXT	("rol", 0x49, 0x59, 0x69, 0x79));
@@ -112,11 +114,12 @@ void InstInfoMap::Initialize()
 	Add(InstInfo::Syntax_INH				("rti", 0x3b));
 	Add(InstInfo::Syntax_INH				("rts", 0x39));
 	Add(InstInfo::Syntax_INH				("sba", 0x10));
-	Add(InstInfo::Syntax_ACC_IMM_DIR_IDX_EXT("sbc", 0x82, 0x92, 0xa2, 0xb2, 0xc2, 0xd2, 0xe2, 0xf2));
+	Add(InstInfo::Syntax_AxB_IMM_DIR_IDX_EXT("sbc", 0x82, 0x92, 0xa2, 0xb2, 0xc2, 0xd2, 0xe2, 0xf2));
 	Add(InstInfo::Syntax_INH				("sec", 0x0d));
 	Add(InstInfo::Syntax_INH				("sei", 0x0f));
 	Add(InstInfo::Syntax_INH				("sev", 0x0b));
-	Add(InstInfo::Syntax_ACC_DIR_IDX_EXT	("sta", 0x97, 0xa7, 0xb7, 0xe7, 0xe7, 0xf7));
+	Add(InstInfo::Syntax_AxB_DIR_IDX_EXT	("sta", 0x97, 0xa7, 0xb7, 0xe7, 0xe7, 0xf7));
+	Add(InstInfo::Syntax_DIR_IDX_EXT		("sts", 0x9f, 0xaf, 0xbf));
 #if 0
 
 
@@ -180,19 +183,8 @@ InstInfo *InstInfo::Syntax_IDX_EXT(const String &symbol, UInt8 codeIDX, UInt8 co
 	return pInstInfo;
 }
 
-InstInfo *InstInfo::Syntax_ACC_IMM_DIR_IDX_EXT(
-	const String &symbol,
-	UInt8 codeIMM_A, UInt8 codeDIR_A, UInt8 codeIDX_A, UInt8 codeEXT_A,
-	UInt8 codeIMM_B, UInt8 codeDIR_B, UInt8 codeIDX_B, UInt8 codeEXT_B)
-{
-	InstInfo *pInstInfo = new InstInfo(symbol);
-	return pInstInfo;
-}
-
-InstInfo *InstInfo::Syntax_ACC_DIR_IDX_EXT(
-	const String &symbol,
-	UInt8 codeDIR_A, UInt8 codeIDX_A, UInt8 codeEXT_A,
-	UInt8 codeDIR_B, UInt8 codeIDX_B, UInt8 codeEXT_B)
+InstInfo *InstInfo::Syntax_DIR_IDX_EXT(
+	const String &symbol, UInt8 codeDIR, UInt8 codeIDX, UInt8 codeEXT)
 {
 	InstInfo *pInstInfo = new InstInfo(symbol);
 	return pInstInfo;
@@ -208,6 +200,24 @@ InstInfo *InstInfo::Syntax_DIR_IDX_IMM_EXT(
 
 InstInfo *InstInfo::Syntax_ACC_ACC_IDX_EXT(
 	const String &symbol, UInt8 codeACC_A, UInt8 codeACC_B, UInt8 codeIDX, UInt8 codeEXT)
+{
+	InstInfo *pInstInfo = new InstInfo(symbol);
+	return pInstInfo;
+}
+
+InstInfo *InstInfo::Syntax_AxB_DIR_IDX_EXT(
+	const String &symbol,
+	UInt8 codeDIR_A, UInt8 codeIDX_A, UInt8 codeEXT_A,
+	UInt8 codeDIR_B, UInt8 codeIDX_B, UInt8 codeEXT_B)
+{
+	InstInfo *pInstInfo = new InstInfo(symbol);
+	return pInstInfo;
+}
+
+InstInfo *InstInfo::Syntax_AxB_IMM_DIR_IDX_EXT(
+	const String &symbol,
+	UInt8 codeIMM_A, UInt8 codeDIR_A, UInt8 codeIDX_A, UInt8 codeEXT_A,
+	UInt8 codeIMM_B, UInt8 codeDIR_B, UInt8 codeIDX_B, UInt8 codeEXT_B)
 {
 	InstInfo *pInstInfo = new InstInfo(symbol);
 	return pInstInfo;
