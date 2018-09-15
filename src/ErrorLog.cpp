50c8a5a761105b0b71d1980cac53b73d0932e5d3
ypsitau <ypsitau@nifty.com>
2018-09-15 14:36:38 +0900

    implementing ErrorLog

--- src/ErrorLog.cpp
+++ src/ErrorLog.cpp
@@ -6,6 +6,46 @@
 //-----------------------------------------------------------------------------
 // ErrorLog
 //-----------------------------------------------------------------------------
+ErrorLog ErrorLog::_instance;
+
+void ErrorLog::AddError(const String &fileName, const char *format, ...)
+{
+	va_list ap;
+	va_start(ap, format);
+	AddErrorV(fileName, 0, format, ap);
+}
+
+void ErrorLog::AddError(const String &fileName, int lineNo, const char *format, ...)
+{
+	va_list ap;
+	va_start(ap, format);
+	AddErrorV(fileName, lineNo, format, ap);
+}
+
+void ErrorLog::AddErrorV(const String &fileName, int lineNo, const char *format, va_list ap)
+{
+	char message[512];
+	::vsprintf_s(message, format, ap);
+	_entryOwner.push_back(new Entry(fileName, lineNo, message));
+}
+
+//-----------------------------------------------------------------------------
+// ErrorLog::Entry
+//-----------------------------------------------------------------------------
+String ErrorLog::Entry::GetString() const
+{
+	String rtn;
+	char buff[128];
+	rtn += _fileName;
+	if (_lineNo == 0) {
+		rtn += ": ";
+	} else {
+		::sprintf_s(buff, ":%d: ", _lineNo);
+		rtn += buff;
+	}
+	rtn += _message;
+	return rtn;
+}
 
 //-----------------------------------------------------------------------------
 // ErrorLog::EntryOwner
--- src/ErrorLog.h
+++ src/ErrorLog.h
@@ -31,9 +31,14 @@ public:
 	};
 private:
 	EntryOwner _entryOwner;
+	static ErrorLog _instance;
 public:
 	inline ErrorLog() {}
 	inline bool HasError() const { return !_entryOwner.empty(); }
+	inline static ErrorLog &GetInstance() { return _instance; }
+	void AddError(const String &fileName, const char *format, ...);
+	void AddError(const String &fileName, int lineNo, const char *format, ...);
+	void AddErrorV(const String &fileName, int lineNo, const char *format, va_list ap);
 };
 
 #endif
