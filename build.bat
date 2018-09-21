5e99b19974acfe80d2ae8b08c9e1e095bb1f5bdf
ypsitau <ypsitau@nifty.com>
2018-09-21 10:56:06 +0900

    updated build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,12 @@
 @echo off
-msbuild jrasm.sln /clp:DisableConsoleColor /t:Rebuild /p:Configuration="Release" /p:Platform=x64
-7za
+set VERSION=0.0.1
+set ZIP=buildtool\7za920\7za.exe
+set WORKDIR=jrasm-%VERSION%
+set ARCFILE=jrasm-%VERSION%.zip
+rem msbuild jrasm.sln /clp:DisableConsoleColor /t:Rebuild /p:Configuration="Release" /p:Platform=x64
+mkdir %WORKDIR%
+copy x64\Release\jrasm.exe %WORKDIR%
+copy README.md %WORKDIR%
+if exist %ARCFILE% del /f %ARCFILE%
+%ZIP% a %ARCFILE% %WORKDIR%\
+rmdir /s /q %WORKDIR%
