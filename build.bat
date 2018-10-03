41b8ce5d5c938d62a3da52814ea459c14cf98df2
ypsitau <ypsitau@nifty.com>
2018-10-03 12:41:52 +0900

    updated version to 0.1.1

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-set VERSION=0.1.0
+set VERSION=0.1.1
 set ZIP=buildtool\7za920\7za.exe
 set WORKDIR=jrasm-%VERSION%
 set ARCFILE=jrasm-%VERSION%.zip
--- src/Version.h
+++ src/Version.h
@@ -4,7 +4,7 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-#define JRASM_VERSION 			"0.1.0"
+#define JRASM_VERSION 			"0.1.1"
 #define JRASM_COPYRIGHT_YEAR	"2018"
 
 #endif
