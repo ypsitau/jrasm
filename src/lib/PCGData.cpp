dd31fad8e4cd2923926363b7744d72d41d5b23ed
ypsitau <ypsitau@nifty.com>
2018-10-06 22:18:55 +0900

    enhanced macros generated by .pcg

--- src/lib/PCGData.cpp
+++ src/lib/PCGData.cpp
@@ -10,10 +10,10 @@ Expr *PCGData::ComposeExpr() const
 {
 	String asmCode;
 	char str[256];
-	do { // create pcg.SYMBOL.put macro
+	do { // create macro: pcg.SYMBOL.put
 		::sprintf_s(str, "pcg.%s.put:\n", GetSymbol());
 		asmCode += str;
-		asmCode += "        .macro\n";
+		asmCode += "        .macro offset=0\n";
 		int charCodePrev = -1;
 		size_t iCol = 0, iRow = 0;
 		for (auto pPCGChar : _pcgCharOwner) {
@@ -32,7 +32,8 @@ Expr *PCGData::ComposeExpr() const
 				::sprintf_s(str, "        ldaa    0x%02x\n", pPCGChar->GetCharCode());
 				asmCode += str;
 			}
-			::sprintf_s(str, "        staa    [x+0x%02x]\n", static_cast<UInt8>(iCol * _xStep + iRow * _yStep));
+			::sprintf_s(str, "        staa    [x+0x%02x+offset]\n",
+						static_cast<UInt8>(iCol * _xStep + iRow * _yStep));
 			asmCode += str;
 			iCol++;
 			if (iCol == _wdChar) {
@@ -43,15 +44,16 @@ Expr *PCGData::ComposeExpr() const
 		}
 		asmCode += "        .end\n";
 	} while (0);
-	do { // create pcg.SYMBOL.erase macro
+	do { // create macro: pcg.SYMBOL.erase
 		::sprintf_s(str, "pcg.%s.erase:\n", GetSymbol());
 		asmCode += str;
-		asmCode += "        .macro\n";
+		asmCode += "        .macro offset=0\n";
 		::sprintf_s(str, "        clra\n");
 		asmCode += str;
 		size_t iCol = 0, iRow = 0;
 		for (size_t i = 0; i < _pcgCharOwner.size(); i++) {
-			::sprintf_s(str, "        staa    [x+0x%02x]\n", static_cast<UInt8>(iCol * _xStep + iRow * _yStep));
+			::sprintf_s(str, "        staa    [x+0x%02x+offset]\n",
+						static_cast<UInt8>(iCol * _xStep + iRow * _yStep));
 			asmCode += str;
 			iCol++;
 			if (iCol == _wdChar) {
@@ -61,20 +63,21 @@ Expr *PCGData::ComposeExpr() const
 		}
 		asmCode += "        .end\n";
 	} while (0);
-	do { // create pcg.SYMBOL.putattr macro
+	do { // create macro: pcg.SYMBOL.putattr
 		::sprintf_s(str, "pcg.%s.putattr:\n", GetSymbol());
 		asmCode += str;
-		::sprintf_s(str, "        .macro foreground,background\n");
+		::sprintf_s(str, "        .macro foreground,background,offset=0\n");
 		asmCode += str;
 		if (_pcgType == PCGTYPE_CRAM) {
-			::sprintf_s(str, "        ldaa    foreground+background*8\n");
+			::sprintf_s(str, "        ldaa    foreground+(background<<3)\n");
 		} else { // _pcgType == PCGTYPE_User
-			::sprintf_s(str, "        ldaa    0x40+foreground+background*8\n");
+			::sprintf_s(str, "        ldaa    (1<<6)+foreground+(background<<3)\n");
 		}
 		asmCode += str;
 		size_t iCol = 0, iRow = 0;
 		for (size_t i = 0; i < _pcgCharOwner.size(); i++) {
-			::sprintf_s(str, "        staa    [x+0x%02x]\n", static_cast<UInt8>(iCol * _xStep + iRow * _yStep));
+			::sprintf_s(str, "        staa    [x+0x%02x+offset]\n",
+						static_cast<UInt8>(iCol * _xStep + iRow * _yStep));
 			asmCode += str;
 			iCol++;
 			if (iCol == _wdChar) {
@@ -84,16 +87,17 @@ Expr *PCGData::ComposeExpr() const
 		}
 		asmCode += "        .end\n";
 	} while (0);
-	do { // create pcg.SYMBOL.eraseattr macro
+	do { // create macro: pcg.SYMBOL.eraseattr
 		::sprintf_s(str, "pcg.%s.eraseattr:\n", GetSymbol());
 		asmCode += str;
-		::sprintf_s(str, "        .macro foreground,background\n");
+		::sprintf_s(str, "        .macro foreground,background,offset\n");
 		asmCode += str;
-		::sprintf_s(str, "        ldaa    foreground+background*8\n");
+		::sprintf_s(str, "        ldaa    foreground+(background<<3)\n");
 		asmCode += str;
 		size_t iCol = 0, iRow = 0;
 		for (size_t i = 0; i < _pcgCharOwner.size(); i++) {
-			::sprintf_s(str, "        staa    [x+0x%02x]\n", static_cast<UInt8>(iCol * _xStep + iRow * _yStep));
+			::sprintf_s(str, "        staa    [x+0x%02x+offset]\n",
+						static_cast<UInt8>(iCol * _xStep + iRow * _yStep));
 			asmCode += str;
 			iCol++;
 			if (iCol == _wdChar) {
