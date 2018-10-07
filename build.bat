641b8576364473d9ba1c5fd57b2f7de198ee8123
ypsitau <ypsitau@nifty.com>
2018-10-07 11:24:19 +0900

    preparing vs project

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 @echo off
-set VERSION=0.1.1
+set VERSION=0.1.2
 set ZIP=buildtool\7za920\7za.exe
 set WORKDIR=jrasm-%VERSION%
 set ARCFILE=jrasm-%VERSION%.zip
 msbuild src\jrasm.sln /clp:DisableConsoleColor /t:Rebuild /p:Configuration="Release" /p:Platform=win32
 mkdir %WORKDIR%
-copy x64\Release\jrasm.exe %WORKDIR%
+copy src\x86\Release\jrasm.exe %WORKDIR%
 copy README.md %WORKDIR%
 if exist %ARCFILE% del /f %ARCFILE%
 %ZIP% a %ARCFILE% %WORKDIR%\
