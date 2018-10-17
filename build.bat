2ce9c0a6fc595cc390695bbfd5872208d663c1bf
ypsitau <ypsitau@nifty.com>
2018-10-17 18:28:01 +0900

    uses VS runtime DLL

--- build.bat
+++ build.bat
@@ -6,6 +6,7 @@ set ARCFILE=jrasm-%VERSION%.zip
 msbuild src\jrasm.sln /clp:DisableConsoleColor /t:Rebuild /p:Configuration="Release" /p:Platform=x86
 if exist %WORKDIR% rmdir /s /q %WORKDIR%
 mkdir %WORKDIR%
+copy src\guest\VC_redist\*.dll %WORKDIR%
 copy src\Release\jrasm.exe %WORKDIR%
 copy README.md %WORKDIR%
 if exist %ARCFILE% del /f %ARCFILE%
