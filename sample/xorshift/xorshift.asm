3761dbfb58b694c522e66a2a69526b3cdeddca71
ypsitau <ypsitau@nifty.com>
2018-10-22 22:13:12 +0900

    implementing xorshift

---
+++ sample/xorshift/xorshift.asm
@@ -0,0 +1,44 @@
+	.org	0x1000
+xrnd:
+	.scope
+	ror	[high]
+	ldaa	[low]
+	rora
+	eora	[high]
+	staa	[high]
+	ror	[low]
+	ldaa	[high]
+	rora
+	eora	[low]
+	staa	[low]
+	eora	[high]
+	staa	[high]
+high:	.equ	$+1
+low:	.equ	$+2
+	ldx	0x0001
+	rts
+	.end
+/*
+xrnd:
+  ld hl,1       ; seed must not be 0
+
+  ld a,h
+  rra
+  ld a,l
+  rra
+  xor h
+  ld h,a
+  ld a,l
+  rra
+  ld a,h
+  rra
+  xor l
+  ld l,a
+	
+  xor h
+  ld h,a
+
+  ld (xrnd+1),hl
+
+  ret
+*/
