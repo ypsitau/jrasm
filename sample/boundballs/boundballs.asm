9b5941623741921067403c99efb6c455880a5a18
ypsitau <ypsitau@nifty.com>
2018-10-23 20:14:23 +0900

    updated boundballs

--- sample/boundballs/boundballs.asm
+++ sample/boundballs/boundballs.asm
@@ -1,5 +1,11 @@
+;;;=============================================================================
+;;; boundballs.asm
+;;;=============================================================================
 	.org	0x1000
 
+;;;-----------------------------------------------------------------------------
+;;; Structure
+;;;-----------------------------------------------------------------------------
 ball:	.struct
 posx:	.ds	@byte
 posy:	.ds	@byte
@@ -7,54 +13,57 @@ dirx:	.ds	@byte
 diry:	.ds	@byte
 	.end
 	
+;;;-----------------------------------------------------------------------------
+;;; Main routine
+;;;-----------------------------------------------------------------------------
 	clra
 	staa	{0x00}		; disable click sound
 	jsr	0xec7f		; clear screen with attribute data in {0x0e}
 	
 	pcgpage.mainpage.store
 
+	;; Initialize parameter table
 	.scope
 	ldx	balls
-loop:
-	; ball.posx = randn(30) + 1
-	randn	30
+each_ball:
+	; ball.posx = xrndn(30) + 1
+	xrndn	30
 	inca
 	staa	[x+ball.posx]
-	; ball.posy = randn(22) + 1
-	randn	22
+	; ball.posy = xrndn(22) + 1
+	xrndn	22
 	inca
 	staa	[x+ball.posy]
-	; ball.dirx = randn(2) * 2 - 1
-	randn	2
+	; ball.dirx = xrndn(2) * 2 - 1
+	xrndn	2
 	asla
 	deca
 	staa	[x+ball.dirx]
-	; ball.diry = randn(2) * 2 - 1
-	randn	2
+	; ball.diry = xrndn(2) * 2 - 1
+	xrndn	2
 	asla
 	deca
 	staa	[x+ball.diry]
+	; next entry
 	addx	@ball
 	cpx	ballsEnd
-	bne	loop
+	bne	each_ball
 	.end
 	
 mainloop:
-
 	.scope
 	ldx	balls
-loop:
+each_ball:
 	.save	x
 	xy2codeaddr [x+ball.posx], [x+ball.posy]
 	pcg.chkcircle2x2.erase
 	code2attraddr
-	pcg.chkcircle2x2.eraseattr 7, 0
+	pcg.chkcircle2x2.eraseattr
 
 	.restore x
 	movebound [x+ball.posx], [x+ball.dirx], 0, 30
 	movebound [x+ball.posy], [x+ball.diry], 0, 22
 
-	.restore x
 	xy2codeaddr [x+ball.posx], [x+ball.posy]
 	pcg.chkcircle2x2.put
 	code2attraddr
@@ -64,25 +73,16 @@ loop:
 	.end
 	addx	4
 	cpx	ballsEnd
-	beq	skip
-	jmp	loop
-skip:
+	jne	each_ball
 	.end
 
-/*
-	ldx	0x10
-delay:
-	dex
-	bne	delay
-*/
-
+	delay	0x10
+	
 	jmp	mainloop
 
 balls:
 	.ds	@ball * 100
 ballsEnd:
-
-	.include "xorshift.inc"
 
 	.pcgpage mainpage,cram:32
 
@@ -107,32 +107,13 @@ ballsEnd:
 
 	.end
 
-;-----------------------------------------------------------------------------
-; Format: addx num
-; Param: num [IMM, DIR, IDX, EXT] .. Added 8bit number
-;-----------------------------------------------------------------------------
-addx:
-	.macro	num
-	stx	[result]
-	ldaa	num
-	clrb
-	adda	[lowbyte]
-	adcb	[highbyte]
-	staa	[lowbyte]
-	stab	[highbyte]
-result:	.equ $+1
-highbyte: .equ $+1
-lowbyte: .equ $+2
-	ldx	0x0000
-	.end
-
-;-----------------------------------------------------------------------------
-; Format: movebound pos,dir,min,max
-; Param: posx [IMM, DIR, IDX, EXT] .. Position
-;        dir [IMM, DIR, IDX, EXT] .. Direction
-;        min [IMM, DIR, IDX, EXT] .. Minimum value
-;        max [IMM, DIR, IDX, EXT] .. Maximum value
-;-----------------------------------------------------------------------------
+;;;-----------------------------------------------------------------------------
+;;; Format: movebound pos,dir,min,max
+;;; Param: posx [IMM, DIR, IDX, EXT] .. Position
+;;;        dir [IMM, DIR, IDX, EXT] .. Direction
+;;;        min [IMM, DIR, IDX, EXT] .. Minimum value
+;;;        max [IMM, DIR, IDX, EXT] .. Maximum value
+;;;-----------------------------------------------------------------------------
 movebound:
 	.macro	pos,dir,min,max
 	ldaa	pos
@@ -152,13 +133,119 @@ rel2:
 	staa	pos
 	.end
 
-;-----------------------------------------------------------------------------
-; Format: xy2codeaddr posx,posy
-; Param: posx [IMM, DIR, IDX, EXT] .. X position
-;        posy [IMM, DIR, IDX, EXT] .. Y position
-; Output: x .. Code address that begins from 0xc100
-; Broken: a, b
-;-----------------------------------------------------------------------------
+;;;-----------------------------------------------------------------------------
+;;; Format: jXX addr16
+;;; Param: addr16 [EXT] .. Address
+;;;-----------------------------------------------------------------------------
+jcc:	.macro addr16
+	bcs	skip
+	jmp	addr16
+skip:	.end
+
+jcs:	.macro addr16
+	bcc	skip
+	jmp	addr16
+skip:	.end
+
+jeq:	.macro addr16
+	bne	skip
+	jmp	addr16
+skip:	.end
+
+jge:	.macro addr16
+	blt	skip
+	jmp	addr16
+skip:	.end
+
+jgt:	.macro addr16
+	ble	skip
+	jmp	addr16
+skip:	.end
+
+jhi:	.macro addr16
+	bls	skip
+	jmp	addr16
+skip:	.end
+
+jle:	.macro addr16
+	bgt	skip
+	jmp	addr16
+skip:	.end
+
+jls:	.macro addr16
+	bhi	skip
+	jmp	addr16
+skip:	.end
+
+jlt:	.macro addr16
+	bge	skip
+	jmp	addr16
+skip:	.end
+
+jmi:	.macro addr16
+	bpl	skip
+	jmp	addr16
+skip:	.end
+
+jne:	.macro addr16
+	beq	skip
+	jmp	addr16
+skip:	.end
+
+jpl:	.macro addr16
+	bmi	skip
+	jmp	addr16
+skip:	.end
+
+jvc:	.macro addr16
+	bvs	skip
+	jmp	addr16
+skip:	.end
+
+jvs:	.macro addr16
+	bvc	skip
+	jmp	addr16
+skip:	.end
+	
+	
+;;;-----------------------------------------------------------------------------
+;;; Format: delay num
+;;; Param: num [IMM, DIR, IDX, EXT] .. Delay duration
+;;;-----------------------------------------------------------------------------
+delay:
+	.macro	num
+	ldx	num
+loop:
+	dex
+	bne	loop
+	.end
+
+;;;-----------------------------------------------------------------------------
+;;; Format: addx num
+;;; Param: num [IMM, DIR, IDX, EXT] .. Added 8bit number
+;;;-----------------------------------------------------------------------------
+addx:
+	.macro	num
+	stx	[result]
+	ldaa	num
+	clrb
+	adda	[lowbyte]
+	adcb	[highbyte]
+	staa	[lowbyte]
+	stab	[highbyte]
+result:	.equ $+1
+highbyte: .equ $+1
+lowbyte: .equ $+2
+	ldx	0x0000
+	.end
+
+;;;-----------------------------------------------------------------------------
+;;; Format: xy2codeaddr posx,posy
+;;; Param: posx [IMM, DIR, IDX, EXT] .. X position
+;;;        posy [IMM, DIR, IDX, EXT] .. Y position
+;;; Output: x .. Code address that begins from 0xc100
+;;; Broken: a, b
+;;;-----------------------------------------------------------------------------
 xy2codeaddr:
 	.macro	posx,posy
 	clra
@@ -179,12 +266,12 @@ lowbyte: .equ $+2
 	ldx	0x0000
 	.end
 
-;-----------------------------------------------------------------------------
-; Format: code2attraddr
-; Input: x .. Code address that begins from 0xc100
-; Output: x .. Attribute address that begins from 0xc500
-; Broken: a
-;-----------------------------------------------------------------------------
+;;;-----------------------------------------------------------------------------
+;;; Format: code2attraddr
+;;; Input: x .. Code address that begins from 0xc100
+;;; Output: x .. Attribute address that begins from 0xc500
+;;; Broken: a
+;;;-----------------------------------------------------------------------------
 code2attraddr:
 	.macro
 	stx	[result]
@@ -195,3 +282,4 @@ result:	.equ $+1
 	ldx	0x0000
 	.end
 
+	.include "xorshift.inc"
