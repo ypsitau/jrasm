45b958c40c56d536378030e1b09ff77505fb5468
ypsitau <ypsitau@nifty.com>
2018-10-26 12:39:36 +0900

    updated sakura

--- sample/mml/mml-sakura.asm
+++ sample/mml/mml-sakura.asm
@@ -1,19 +1,12 @@
 //-----------------------------------------------------------------------------
 	.org	0x1000
 	
-	.mml	"r1 aab2 aab2 ab cb ab8a8f2 ecef"
-	.mml	"ee8c8<b2> ab cb ab8a8f2 ecef ee8c8<b2>"
-	.mml	"aab2 aab2 ref2 b8a8fe2 ;"
+	.mml	">dde2 dde2 defe de8d8<b-2 afab-"
+	.mml	"aa8f8e2 >defe de8d8<b-2 afab- aa8f8e2"
+	.mml	">dde2 dde2 r8<a8b-2>e8d8 <b-1 a1;"
 
 //-----------------------------------------------------------------------------
-	.org	0x1100
 
-	.mml	"r1 l8 r2r e cb r2r e cb rererere rererbaferr4r2"
-	.mml	"r2r e f e rererere rererbaf err4r2 r2r e f e"
-	.mml	"r2r e cb r2r e cb r4refege f4a4b2 ;"
-
-//-----------------------------------------------------------------------------
-	.org	0x1200
-	.mml	"r1 l2 <a>e <a>e <a>a f<b> <a>e"
-	.mml	"<a>e <a>a f<b> <a>e <a>e"
-	.mml	"<a>e <a>e f<a> <b>e ;"
+	.mml	"afe2 afe2 d2.a f2e2 d2.f"
+	.mml	"d1 a2de f2e2 d2.e d1"
+	.mml	"afe2 afe2 feda f2.a8f8 e1;"
