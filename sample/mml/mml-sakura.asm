425bce91150d18b1673d83eb0677e7901803bdda
ypsitau <ypsitau@nifty.com>
2018-10-26 12:04:40 +0900

    added mml sample

---
+++ sample/mml/mml-sakura.asm
@@ -0,0 +1,19 @@
+//-----------------------------------------------------------------------------
+	.org	0x1000
+	
+	.mml	"r1 aab2 aab2 ab cb ab8a8f2 ecef"
+	.mml	"ee8c8<b2> ab cb ab8a8f2 ecef ee8c8<b2>"
+	.mml	"aab2 aab2 ref2 b8a8fe2 ;"
+
+//-----------------------------------------------------------------------------
+	.org	0x1100
+
+	.mml	"r1 l8 r2r e cb r2r e cb rererere rererbaferr4r2"
+	.mml	"r2r e f e rererere rererbaf err4r2 r2r e f e"
+	.mml	"r2r e cb r2r e cb r4refege f4a4b2 ;"
+
+//-----------------------------------------------------------------------------
+	.org	0x1200
+	.mml	"r1 l2 <a>e <a>e <a>a f<b> <a>e"
+	.mml	"<a>e <a>a f<b> <a>e <a>e"
+	.mml	"<a>e <a>e f<a> <b>e ;"
