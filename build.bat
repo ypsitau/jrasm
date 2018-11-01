aae43724ec1ed00259c8a8edbce61a35cfe42520
ypsitau <ypsitau@nifty.com>
2018-11-01 12:36:14 +0900

    updated version to 1.0.0

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-set VERSION=0.3.1
+set VERSION=1.0.0
 set ZIP=src\guest\buildtool\7za920\7za.exe
 set WORKDIR=jrasm-%VERSION%
 set ARCFILE=jrasm-%VERSION%.zip
@@ -7,9 +7,11 @@ if "%1" == "release" (
 	msbuild src\jrasm.sln /clp:DisableConsoleColor /t:Rebuild /p:Configuration="Release" /p:Platform=x86
 	if exist %WORKDIR% rmdir /s /q %WORKDIR%
 	mkdir %WORKDIR%
+	mkdir %WORKDIR%\inc
 	copy src\guest\VC_redist\*.dll %WORKDIR%
 	copy src\Release\jrasm.exe %WORKDIR%
 	copy README.md %WORKDIR%
+	copy inc\* %WORKDIR%\inc
 	if exist %ARCFILE% del /f %ARCFILE%
 	%ZIP% a %ARCFILE% %WORKDIR%\
 	rmdir /s /q %WORKDIR%
