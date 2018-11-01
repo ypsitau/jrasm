62b30a7011243fe91c0fb6ec73169132bcdf3043
ypsitau <ypsitau@nifty.com>
2018-11-01 21:44:01 +0900

    updated sample

--- sample/bios/bios-pick.asm
+++ sample/bios/bios-pick.asm
@@ -3,28 +3,8 @@
 	bios.keyclick 0
 loop:
 	bios.pick
-	tsta
-	beq	loop
-	cmpa	0x1c
-	bne	rel1
-	ldaa	0x89
-	bra	rel4
-rel1:
-	cmpa	0x1d
-	bne	rel2
-	ldaa	0x86
-	bra	rel4
-rel2:
-	cmpa	0x1e
-	bne	rel3
-	ldaa	0x88
-	bra	rel4
-rel3:
-	cmpa	0x1f
-	bne	rel4
-	ldaa	0x87
-rel4:
-	bios.putc.acca
+	bios.puthex.acca
+	bios.puts "  \0"
 	bra	loop
 
 	.include "bios.inc"
