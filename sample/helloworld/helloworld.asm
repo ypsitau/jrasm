5c41bd69bb824390404e98039c75b471c1a69631
ypsitau <ypsitau@nifty.com>
2018-11-03 11:49:56 +0900

    updated sample

--- sample/helloworld/helloworld.asm
+++ sample/helloworld/helloworld.asm
@@ -1,20 +1,20 @@
-	.ORG	0x1000
+        .ORG    0x1000
 loop:
-	LDX	[ptr_src]
-	LDAA	[x]
-	BEQ	done
-	INX
-	STX	[ptr_src]
-	LDX	[ptr_dst]
-	STAA	[x]
-	INX
-	STX	[ptr_dst]
-	BRA	loop
+        LDX     [ptr_src]
+        LDAA    [X]
+        BEQ     done
+        INX
+        STX     [ptr_src]
+        LDX     [ptr_dst]
+        STAA    [X]
+        INX
+        STX     [ptr_dst]
+        BRA     loop
 done:
-	RTS
+        RTS
 ptr_src:
-	.DW	hello_world
+        .DW     hello_world
 ptr_dst:
-	.DW	0xc100 + 9 + 20 * 0x20
+        .DW     0xc100 + 9 + 20 * 0x20
 hello_world:
-	.DB	"Hello, world!", 0
+        .DB     "Hello, world!", 0
