73b4864b0bf766d9b8b29a3a03b98594d7ee0a0c
ypsitau <ypsitau@nifty.com>
2018-11-05 13:17:21 +0900

    implementing oputil.inc

--- sample/oputil/oputil-test.asm
+++ sample/oputil/oputil-test.asm
@@ -3,7 +3,9 @@
 ;;;=============================================================================
 	.cseg
 	.org	0x1000
-
+	;;-----------------------
+	;; addmb
+	;;-----------------------
 	jsr	init
 	bios.puthex.mb [byte1]
 	bios.putc '+'
@@ -13,16 +15,98 @@
 	bios.puthex.mb [byte1]
 	bios.putc '\r'
 	
+	;;-----------------------
+	;; addmw
+	;;-----------------------
 	jsr	init
 	bios.puthex.mw [word1]
 	bios.putc '+'
 	bios.puthex.mb [byte2]
 	addmw.mb [word1],[byte2]
 	bios.putc '='
-	bios.puthex.mb [word1]
+	bios.puthex.mw [word1]
+	bios.putc '\r'
+
+	jsr	init
+	bios.puthex.mw [word1]
+	bios.putc '+'
+	bios.puthex.mw [word2]
+	addmw.mw [word1],[word2]
+	bios.putc '='
+	bios.puthex.mw [word1]
+	bios.putc '\r'
+
+	jsr	init
+	bios.puthex.mw [word1]
+	bios.putc '+'
+	bios.puthex.mb 0x34
+	addmw.mb [word1],0x34
+	bios.putc '='
+	bios.puthex.mw [word1]
+	bios.putc '\r'
+
+	jsr	init
+	bios.puthex.mw [word1]
+	bios.putc '+'
+	bios.puthex.mw 0x12f4
+	addmw.mw [word1],0x12f4
+	bios.putc '='
+	bios.puthex.mw [word1]
+	bios.putc '\r'
+
+	;;-----------------------
+	;; submb
+	;;-----------------------
+	jsr	init
+	bios.puthex.mb [byte1]
+	bios.putc '-'
+	bios.puthex.mb [byte2]
+	submb.mb [byte1],[byte2]
+	bios.putc '='
+	bios.puthex.mb [byte1]
+	bios.putc '\r'
+	
+	;;-----------------------
+	;; submw
+	;;-----------------------
+	jsr	init
+	bios.puthex.mw [word1]
+	bios.putc '-'
+	bios.puthex.mb [byte2]
+	submw.mb [word1],[byte2]
+	bios.putc '='
+	bios.puthex.mw [word1]
+	bios.putc '\r'
+
+	jsr	init
+	bios.puthex.mw [word1]
+	bios.putc '-'
+	bios.puthex.mw [word2]
+	submw.mw [word1],[word2]
+	bios.putc '='
+	bios.puthex.mw [word1]
+	bios.putc '\r'
+
+	jsr	init
+	bios.puthex.mw [word1]
+	bios.putc '-'
+	bios.puthex.mb 0x34
+	submw.mb [word1],0x34
+	bios.putc '='
+	bios.puthex.mw [word1]
+	bios.putc '\r'
+
+	jsr	init
+	bios.puthex.mw [word1]
+	bios.putc '-'
+	bios.puthex.mw 0x12f4
+	submw.mw [word1],0x12f4
+	bios.putc '='
+	bios.puthex.mw [word1]
 	bios.putc '\r'
 
 	rts
+
 	addmb	[byte1],8
 	addmb	[byte1],[byte2]
 	
@@ -40,10 +124,10 @@
 	rts
 
 init:	
-	ldmb	[byte1],0x12
-	ldmb	[byte2],0xf4
-	ldmw	[word1],0x1234
-	ldmw	[word2],0x56f8
+	ldmb	[byte1],0xc3
+	ldmb	[byte2],0x34
+	ldmw	[word1],0x5678
+	ldmw	[word2],0x12f4
 	rts
 
 	.wseg
