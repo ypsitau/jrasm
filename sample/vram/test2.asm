af7cbab49b5ccc406c61f9455e0d816fdf13f0e4
ypsitau <ypsitau@nifty.com>
2018-11-13 12:17:39 +0900

    implementing vram

--- sample/vram/test2.asm
+++ sample/vram/test2.asm
@@ -1,8 +1,15 @@
 	.org	0x1000
 
 	vram.clear
+	ldmb	[0xca00],1
+	vram.color 7,0
+	vram.fromxy 0,0
+	vram.fill '*',32
+	vram.color 7,0
+	vram.fromxy 0,23
+	vram.fill '*',32
 loop:
-	vram.scrollup
+	vram.scrollup 1,22
 	xrndn.mb 31
 	staa	[posx]
 	vram.fromxy [posx],22
@@ -16,5 +23,6 @@ loop:
 	.wseg
 posx:	.ds	1
 
+	.include "oputil.inc"
 	.include "xrnd.inc"
 	.include "vram32x24x2.inc"
