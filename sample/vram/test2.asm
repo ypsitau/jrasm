df34d3f7151bbcf1d40dd61d08cda04ab10167a3
ypsitau <ypsitau@nifty.com>
2018-11-14 13:46:20 +0900

    implementing vram

--- sample/vram/test2.asm
+++ sample/vram/test2.asm
@@ -3,14 +3,47 @@
 	vram.clear
 	ldmb	[0xca00],1
 	vram.color 7,0
+
+/*
 	vram.fromxy 0,0
 	vram.fill '*',32
 	vram.color 7,0
-	vram.fromxy 0,23
+	vram.fromxy 2,23
 	vram.fill '*',32
-	vram.fromxy 0,0
+	vram.fromxy 2,0
 	vram.puts "Hello\0"
-loop:
+	vram.fromxy 2,2
+	vram.puts "Hello\0"
+
+	vram.fromxy 0,10
+	vram.puts "ABCDEFG\0"
+
+	vram.fromxy 0,22
+	vram.puts "ABCDEFG\0"
+	//vram.fill '+',20
+*/
+
+	vram.fromxy 0,1
+	vram.puts "ABCD\0"
+
+	vram.fromxy 32,1
+	vram.puts "ABCD\0"
+
+	vram.fromxy 0,2
+	vram.puts "ABCD\0"
+
+	vram.fromxy 32,2
+	vram.puts "ABCD\0"
+
+	vram.fromxy 0,3
+	vram.puts "ABCD\0"
+
+	vram.fromxy 32,3
+	vram.puts "ABCD\0"
+
+	vram.refresh
+loop:	bra	loop
+
 	vram.scrolldown 1,22
 	xrndn.mb 32
 	staa	[posx]
@@ -27,4 +60,4 @@ posx:	.ds	1
 
 	.include "oputil.inc"
 	.include "xrnd.inc"
-	.include "vram32x24x3.inc"
+	.include "vram40x24x2.inc"
